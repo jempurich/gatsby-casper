7a0b82b6801ada6712251a121ac83effe121f231
jempurich <jempurich@gmail.com>
2018-12-23 03:18:13 +0800

    Install GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,22 +63,18 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-XXXX-Y',
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // IP anonymization for GDPR compliance
-        anonymize: true,
-        // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Specifies what percentage of users should be tracked
-        sampleRate: 100,
-        // Determines how often site speed tracking beacons will be sent
-        siteSpeedSampleRate: 10,
-      },
-    },
+        resolve: `gatsby-plugin-google-tagmanager`,
+        options: {
+          id: "GTM-W9Q9W9F",
+
+          // Include GTM in development.
+          // Defaults to false meaning GTM will only be loaded in production.
+          includeInDevelopment: false,
+
+          // Specify optional GTM environment details.
+          gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+          gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+        },
+      }
   ],
 };
