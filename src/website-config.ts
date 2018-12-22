55aa84d30217f17210561e0eaa1380b013ce9f2d
jempurich <jempurich@gmail.com>
2018-12-23 03:20:49 +0800

    no message

--- src/website-config.ts
+++ src/website-config.ts
@@ -36,7 +36,7 @@ const config: WebsiteConfig = {
   lang: 'th',
   siteUrl: 'https://purich.co',
   facebook: 'https://www.facebook.com/jempurich',
-  twitter: '',
+  twitter: 'https://twitter.com/jempurich',
   showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
