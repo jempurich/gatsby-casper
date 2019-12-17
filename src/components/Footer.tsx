35b9599d0f45e7f1d040f784dace75baa05c3c63
jempurich <jempurich@gmail.com>
2019-12-18 00:03:46 +0800

    removed twitter link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -84,16 +84,6 @@ const Footer: React.FC = () => {
               Facebook
             </a>
           )}
-          {config.twitter && (
-            <a href={config.twitter} target="_blank" rel="noopener noreferrer">
-              Twitter
-            </a>
-          )}
-
-          <a href="https://ghost.org" target="_blank" rel="noopener noreferrer">
-            Ghost
-          </a>
-
           <a href="/rss.xml">RSS</a>
         </SiteFooterNav>
       </div>
