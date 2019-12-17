98958379db5b47903128f4335797c00cac23d132
jempurich <jempurich@gmail.com>
2019-12-17 23:44:24 +0800

    no message

--- src/website-config.ts
+++ src/website-config.ts
@@ -62,7 +62,7 @@ const config: WebsiteConfig = {
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: 'Craft with ❤️ by Purich',
+  footer: '👨‍💻 with ❤️ by Purich',
 };
 
 export default config;
