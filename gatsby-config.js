250c0e8cb9968a7e09283d2b1babb5ea39de5b4f
jempurich <jempurich@gmail.com>
2019-12-17 23:10:56 +0800

    updated pagination

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Purich',
+    description: 'Just another stupid blog',
+    siteUrl: 'https://purich.co', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://purich.co',
       },
     },
     'gatsby-plugin-emotion',
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-68637067-6',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
--- src/website-config.ts
+++ src/website-config.ts
@@ -49,20 +49,20 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Purich',
+  description: 'Just another stupid blog',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
-  showSubscribe: true,
+  siteUrl: 'https://purich.co',
+  facebook: 'https://www.facebook.com/jempurich',
+  twitter: 'https://twitter.com/jempurich',
+  showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: 'is based on Gatsby Casper',
+  footer: 'Craft with ❤️ by Purich',
 };
 
 export default config;
