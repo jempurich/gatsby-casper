ab04745d555cef9eea757d63644ee5fb394ca6a8
jempurich <jempurich@gmail.com>
2019-12-18 00:18:08 +0800

    remove gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,28 +41,6 @@ module.exports = {
             },
           },
         ],
-        plugins: [
-          {
-            resolve: "gatsby-plugin-google-tagmanager",
-            options: {
-              id: "GTM-W9Q9W9F",
-
-              // Include GTM in development.
-              // Defaults to false meaning GTM will only be loaded in production.
-              includeInDevelopment: false,
-
-              // datalayer to be set before GTM is loaded
-              // should be an object or a function that is executed in the browser
-              // Defaults to null
-              defaultDataLayer: { platform: "gatsby" },
-
-              // Specify optional GTM environment details.
-              gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-              gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-              dataLayerName: "YOUR_DATA_LAYER_NAME",
-            },
-          },
-        ],
       },
     },
     'gatsby-transformer-json',
