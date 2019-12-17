96156c826fcb7e498e25cb370b0edbbde6540cf0
jempurich <jempurich@gmail.com>
2019-12-18 00:12:02 +0800

    add gtm id

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
           {
             resolve: "gatsby-plugin-google-tagmanager",
             options: {
-              id: "YOUR_GOOGLE_TAGMANAGER_ID",
+              id: "W9Q9W9F",
 
               // Include GTM in development.
               // Defaults to false meaning GTM will only be loaded in production.
